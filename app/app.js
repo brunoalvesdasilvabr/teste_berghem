bfc731f2af8b29e9575ee2bd38b5f7ce7da9557c
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2019-11-20 11:45:47 -0300

    adicionando a busca de paises pelo id da  busca na api

--- app/app.js
+++ app/app.js
@@ -3,22 +3,48 @@ var clashOfClansRank = angular.module('clashOfClansRank', [])
 clashOfClansRank.controller('clashOfClansRank', ['$scope', function ($scope) {
     $scope.table = false;
     $scope.loading = false;
-    $scope.country = "none" //variavel que receberá o id do pais;
+    $scope.valor_selected ;//variavel que receberá o id do pais;
     //Função que receberá os dados da api
-    $scope.getData = async function () {
-        $scope.country
-        let response = await axios('data_api/data_api.php', {
+    $scope.getData = function () {
+        console.log($scope.country.id)
+        axios('data_api/data_api.php', {
             //aqui eu passo o id do pais que veio pelo select
             params: {
-                country: $scope.country
+                country: $scope.country.id
             }
-        });
-        //variavel de resposta do php
-        $scope.rankings = response.data.items;
+        }).then(function(response){
+            $scope.rankings = response.data.items;
+        $scope.$apply();
         console.log($scope.rankings)
 
+        })
+        //variavel de resposta do php
+        
+
     }
-    // $scope.getData()
+
+    $scope.getCountry = function () {
+        $scope.countries = [];
+       axios('data_api/country_data.php')
+       .then(function(response){
+                //variavel de resposta do php 
+        let data = response.data.items;
+
+        data.forEach(function(country){
+            if(country.isCountry == true){
+                $scope.countries.push(country)  
+            }
+        })
+        $scope.$apply();
+       })
+   
+     return  $scope.countries
+
+    }
+
+    $scope.getCountry();
+
+   
 
 
     $scope.showTable = function () {
---
+++ data_api/country_data.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+$clantag = "#P8PPLJ2";
+
+$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhmYTUwODkxLTFkYmYtNDNjNi04MDMzLWU5MGUzYjZhNGU4YSIsImlhdCI6MTU3NDA2MTQ1Nywic3ViIjoiZGV2ZWxvcGVyLzYxZDZjMGIxLTA5MzgtZWI4Yi04ZGUwLTRjZTgyZWNiZmI0YiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4Ny4zNy43LjExNSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.v3vY3XMPlAfFDuP3Fl3iqE2ByeP3a7N5Wmh63WXhz3Dh70syQ6kTOKguTsKN0edYjmdE8U_AVICtcwamnZ93qg";
+$url = "https://api.clashofclans.com/v1/locations";
+
+$ch = curl_init($url);
+
+$headr = array();
+$headr[] = "Accept: application/json";
+$headr[] = "Authorization: Bearer ".$token;
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+$res = curl_exec($ch);
+ $data = $res;
+curl_close($ch);
+
+// echo "<pre>";
+print_r($data);
